86759c6ea6e5631d5d3caab98c5674681d692f7b
jongja <whwhdgus94@naver.com>
2020-08-20 11:20:51 +0900

    Upload linked list

---
+++ Study/cpp/Linked_list/llist.cpp
@@ -0,0 +1,50 @@
+/* DOUBLE LINKED LIST
+ *
+ * Implement only insert(), delete(), search() functions
+ * Using Static allocate
+ *
+ */
+
+typedef struct LinkedListNodes
+{
+    int val;
+    struct LinkedListNodes* next;
+    struct LinkedListNodes* prev;
+
+}node;
+
+void delete_list(int val) {
+    node* walker = head;
+    while (walker) {
+        if (walker->val == val) { // if delete node is head
+            if (walker == head) {
+                head = head->next;
+                head->prev = NULL;
+            }
+            else { // if delete node is middle or last
+                walker->prev->next = walker->next;
+                if (walker->next) // if walker's next is NULL (Last node)
+                    walker->next->prev = walker->prev;
+            }
+            return;
+        }
+        walker = walker->next;
+    }
+}
+
+void insert_list(int val) {
+    node* newNode = salloc();
+    newNode->next = NULL;
+    newNode->prev - NULL;
+    newNode->val = val;
+
+    if (!head) { 
+        head = newNode;
+        return;
+    }
+    else {
+        head->prev = newNode;
+        newNode->next = head;
+        head = newNode;
+    }
+}
