37caefd7190d83e47c48b82006e94f6918605cc9
jongja <whwhdgus94@naver.com>
2020-08-24 21:26:21 +0900

    CFS scheduler update

--- Study/system/cfs_scheduler.c
+++ Study/system/cfs_scheduler.c
@@ -1,30 +1,87 @@
-/* CFS scheduler
+/* CFS scheduler simulator
  *
  * This code include slicetime, vruntime of CFS scheduler.
  * It was made by referring on actual CFS kernel code ( ~/kernel/sched/fair.c)
- * 
+ *
  */
 
 #include <stdio.h>
-
-static const int prio_to_weight[40] = {
-/* -20 */     88761,     71755,     56483,     46273,     36291,
-/* -15 */     29154,     23254,     18705,     14949,     11916,
-/* -10 */      9548,      7620,      6100,      4904,      3906,
-/*  -5 */      3121,      2501,      1991,      1586,      1277,
-/*   0 */      1024,       820,       655,       526,       423,
-/*   5 */       335,       272,       215,       172,       137,
-/*  10 */       110,        87,        70,        56,        45,
-/*  15 */        36,        29,        23,        18,        15,
-};
-
-struct sched_entity{
-	int on_rq;
-	int load;
-	int inv_load;
-};
+#include "cfs_scheduler.h"
 
 
-int main(void){
+u64 __sched_period(int nr_running) {
+	if (nr_running > sched_nr_latency)
+		return nr_running * sysctl_sched_min_granularity;
+	else
+		return sysctl_sched_latency;
+}
+
+void update_load_add(struct load_weight* lw, unsigned long inc) {
+	lw->weight += inc;
+	lw->inv_weight = 0;
+}
+
+void __update_inv_weight(struct load_weight* lw) {
+	unsigned long w;
+	if (lw->inv_weight) return;
+	w = lw->weight;
+	lw->inv_weight = WMULT_CONST / w;
+}
+
+// Function for calculate slice time of task.
+// This function can replace with this formula. (KERNEL USE)
+// actual_exec_time * ((weight(task) * inv_weight(cfs_rq)) >> 32)
+u64 __calc_delta(u64 delta_exec, unsigned long weight, struct load_weight* lw) {
+	int shift = WMULT_SHIFT;
+	__update_inv_weight(lw);
+	return (delta_exec * weight * lw->inv_weight) >> shift;
+}
+
+// Function for calculte time slice of task
+u64 sched_slice(struct cfs_rq* cfs_rq, struct sched_entity* se) {
+	u64 slice = __sched_period(cfs_rq->nr_running + !se->on_rq);
+	struct load_weight lw;
+	struct load_weight* load;
+
+	load = &cfs_rq->load;
+
+	if (!se->on_rq) {
+
+		lw = cfs_rq->load;
+		update_load_add(&lw, se->load.weight);
+		load = &lw;
+		cfs_rq->load.weight = lw.weight;
+
+		se->on_rq = 1; // not here..
+	}
+	slice = __calc_delta(slice, se->load.weight, load);
+
+	return slice;
+}
+
+void init() {
+	cfs_queue = &cfs_pool;
+	cfs_queue->nr_running = 0;
+	cfs_queue->load.weight = 0;
+	cfs_queue->load.inv_weight = 0;
+	cfs_queue->curr = NULL;
+}
+
+
+int main(void) {
+	init();
+	int nice;
+	printf("Nice > ");
+	scanf("%d", &nice);
+
+	struct sched_entity* se = se_alloc();
+	se->nice = nice;
+	se->load.weight = prio_to_weight[nice + 20];
+	se->load.inv_weight = WMULT_CONST / se->load.weight;
+	se->on_rq = 0;
+	se->cfs_rq = cfs_queue;
+
+	printf("%lld\n", sched_slice(cfs_queue, se));
+
 	return 0;
 }
