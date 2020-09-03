e0c39810794e270f5bb9a1751e5a4d0ec4e195fe
jongja <whwhdgus94@naver.com>
2020-09-03 22:48:05 +0900

    Disjoint Set

---
+++ Study/cpp/DisjointSet/disjoint_set.cpp
@@ -0,0 +1,27 @@
+/* Disjoint Set
+ *
+ * This code is Disjoint Set of union and find function
+ * array p need to initialize the value '-1'
+ */
+
+#define MAX_UNION_SIZE 200000
+
+int p[MAX_UNION_SIZE] = { 0 }; // init to '-1'
+
+int find(int n) {
+	if (p[n] < 0) return n;
+	else {
+		p[n] = find(p[n]);
+		return p[n];
+	}
+}
+
+void uni(int a, int b) {
+
+	int ap = find(a);
+	int bp = find(b);
+
+	if (ap != bp) {
+		p[bp] = ap;
+	}
+}
