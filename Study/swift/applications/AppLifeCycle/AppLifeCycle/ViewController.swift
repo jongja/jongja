d358e1a66fd0fe14573f2049d1664ea9369ab2da
jongja <whwhdgus94@naver.com>
2020-09-08 00:20:52 +0900

    Upload app life cycle

---
+++ Study/swift/applications/AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by 종자 on 2018. 7. 13..
+//  Copyright © 2018년 Jongja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
