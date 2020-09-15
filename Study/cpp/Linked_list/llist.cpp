927ec64e48741dc5a4f5705383b16bba6f947924
jongja <whwhdgus94@naver.com>
2020-09-15 22:20:51 +0900

    update codes

--- Study/cpp/Linked_list/llist.cpp
+++ Study/cpp/Linked_list/llist.cpp
@@ -17,7 +17,7 @@ void delete_list(int val) {
     node* walker = head;
     while (walker) {
         if (walker->val == val) { // if delete node is head
-            if (walker == head) {
+            if (!walker->prev) { // if head node,
                 head = head->next;
                 head->prev = NULL;
             }
@@ -35,7 +35,7 @@ void delete_list(int val) {
 void insert_list(int val) {
     node* newNode = salloc();
     newNode->next = NULL;
-    newNode->prev - NULL;
+    newNode->prev = NULL;
     newNode->val = val;
 
     if (!head) { 
