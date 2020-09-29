a1283675968fb161d8c8dd89707736dca452bd8f
jongja <whwhdgus94@naver.com>
2020-09-30 00:01:25 +0900

    Fix heap

--- Study/cpp/Heap/heap.cpp
+++ Study/cpp/Heap/heap.cpp
@@ -18,7 +18,7 @@ typedef struct Heap { // Modulization.
 	}
 	void push(int val) { // max heap push finction
 		if (size == MAX_HEAP_SIZE) return;
-		int cur = ++heap_size;
+		int cur = ++size;
 
 		while (cur != 1 && heap[cur >> 1] < val) { // if we change this "<" to ">", this code become Min heap.
 			heap[cur] = heap[cur >> 1];
@@ -27,13 +27,13 @@ typedef struct Heap { // Modulization.
 		heap[cur] = val;
 	}
 	int pop(void) {
-		if (!heap_size) return -1;
+		if (!size) return -1;
 		int res = heap[1];
 
-		int e = heap[heap_size--];
+		int e = heap[size--];
 		int cur = 1, child = 2;
-		while (child <= heap_size) {
-			if (child < heap_size && heap[child] < heap[child + 1]) child++; // if we change this "<" to ">", this code become Min heap.
+		while (child <= size) {
+			if (child < size && heap[child] < heap[child + 1]) child++; // if we change this "<" to ">", this code become Min heap.
 			if (heap[child] <= e) break; // if we change this "<=" to ">=", this code become Min heap.
 			heap[cur] = heap[child];
 			cur = child; child <<= 1;
