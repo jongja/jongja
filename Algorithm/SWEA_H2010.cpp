5a0525ee46aec3fdaa544d3fb79cb9454045ebe2
jongja <whwhdgus94@naver.com>
2020-10-27 22:21:21 +0900

    completed!

--- Algorithm/SWEA_H2010.cpp
+++ Algorithm/SWEA_H2010.cpp
@@ -1,15 +1,16 @@
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
+/* Completed!
+ *
+ */
+
 
 #include <stdio.h>
 
 #define NULL 0
-#define MAXE 200000
-#define MAXN 1000
-#define MAXG 100
+#define MAXE 60001
+#define MAXN 1001
+#define MAXG 101
 #define MAXLEN 20
-#define MAXH 100003
+#define MAXH 7003
 #define PN 23
 #define salloc() &node[sid];
 
@@ -26,21 +27,24 @@ struct Event
 	Event* h_prev;
 	Event* u_next;
 	Event* u_prev;
-}node[MAXE], *htable[MAXH];
+}node[MAXE], * htable[MAXG][MAXH];
 
 struct User {
 	int cnt;
 	Event* events;
 }u[MAXN];
 
+int tarray[5] = { 0 };
+int tarray_cnt = 0;
+
 
 /* STRING FUNCTIONS */
-void _strcpy(char dst[], const char src[]) {
+void _strcpy(register char dst[], register const char src[]) {
 	int c = 0;
 	while ((dst[c] = src[c]) != 0) ++c;
 }
 
-int _strcmp(const char str1[], const char str2[]) {
+int _strcmp(register const char str1[], register const char str2[]) {
 	int c = 0;
 	while (str1[c] != 0 && str1[c] == str2[c]) ++c;
 	return str1[c] - str2[c];
@@ -52,24 +56,24 @@ int _strcmp(const char str1[], const char str2[]) {
 /* HASH FUNCTIONS */
 int hashing(char* s) {
 	unsigned int key = 0, step = 1;
-	for (int i = 0; s[i]; i++) {
+	for (register int i = 0; s[i]; i++) {
 		key += step * s[i];
 		step *= PN;
 	}
 	return key % MAXH;
 }
 
-void insert_hash(Event* newNode) {
+void insert_hash(int gid, Event* newNode) {
 	int key = hashing(newNode->name);
-	if (!htable[key]) {
-		htable[key] = newNode;
+	if (!htable[gid][key]) {
+		htable[gid][key] = newNode;
 		newNode->master = true;
 	}
 	else {
-		Event* walker = htable[key];
+		register Event* walker = htable[gid][key];
 		bool canImaster = true;
 		while (walker) {
-			if (_strcmp(walker->name, newNode->name)) canImaster = false;
+			if (!_strcmp(walker->name, newNode->name)) canImaster = false;
 			if (!walker->h_next) { // go to last node.
 				walker->h_next = newNode;
 				newNode->h_prev = walker;
@@ -82,17 +86,19 @@ void insert_hash(Event* newNode) {
 	return;
 }
 
-Event* delete_hash(int uid, char* name) {
+Event* delete_hash(int uid, int gid, register char* name) {
 	int key = hashing(name);
-	Event* dnode = NULL;
-	Event* walker = htable[key];
+	register Event* dnode = NULL;
+	register Event* walker = htable[gid][key];
+	if (!walker) return NULL;
 	while (walker) {
 		if (!_strcmp(walker->name, name) && uid == walker->uid) {
 			dnode = walker;
 			if (!walker->h_prev) { // if dnode is head
-				htable[key] = walker->h_next;
-				walker->h_next->h_prev = NULL;
-			} 
+				htable[gid][key] = walker->h_next;
+				if (walker->h_next)
+					walker->h_next->h_prev = NULL;
+			}
 			else if (!walker->h_next) { // if dnode is last node
 				walker->h_prev->h_next = NULL;
 			}
@@ -104,8 +110,37 @@ Event* delete_hash(int uid, char* name) {
 		}
 		walker = walker->h_next;
 	}
+	if (dnode)
+		dnode->h_next = dnode->h_prev = NULL;
+	return dnode;
+}
 
-	dnode->h_next = dnode->h_prev = NULL;
+Event* delete_hash2(int gid, register char* name) {
+	int key = hashing(name);
+	register Event* dnode = NULL;
+	register Event* walker = htable[gid][key];
+	if (!walker) return NULL;
+	while (walker) {
+		if (!_strcmp(walker->name, name)) {
+			dnode = walker;
+			if (!walker->h_prev) { // if dnode is head
+				htable[gid][key] = walker->h_next;
+				if (walker->h_next)
+					walker->h_next->h_prev = NULL;
+			}
+			else if (!walker->h_next) { // if dnode is last node
+				walker->h_prev->h_next = NULL;
+			}
+			else {
+				walker->h_prev->h_next = walker->h_next;
+				walker->h_next->h_prev = walker->h_prev;
+			}
+			break;
+		}
+		walker = walker->h_next;
+	}
+	if (dnode)
+		dnode->h_next = dnode->h_prev = NULL;
 	return dnode;
 }
 
@@ -118,7 +153,7 @@ void insert_user(int uid, Event* newNode) {
 		u[uid].events = newNode;
 	}
 	else {
-		Event* walker = u[uid].events;
+		register Event* walker = u[uid].events;
 		while (walker) {
 			if (!walker->u_next) { // go to last node.
 				walker->u_next = newNode;
@@ -128,18 +163,21 @@ void insert_user(int uid, Event* newNode) {
 			walker = walker->u_next;
 		}
 	}
+	u[uid].cnt++;
 	return;
 }
 
-Event* delete_user(int uid, char* name) {
-	Event* dnode = NULL;
-	Event* walker = u[uid].events;
+Event* delete_user(int uid, register char* name) {
+	register Event* dnode = NULL;
+	register Event* walker = u[uid].events;
+	if (!walker) return NULL;
 	while (walker) {
 		if (!_strcmp(walker->name, name)) {
 			dnode = walker;
 			if (!walker->u_prev) { // if dnode is head
 				u[uid].events = walker->u_next;
-				walker->u_next->u_prev = NULL;
+				if (walker->u_next)
+					walker->u_next->u_prev = NULL;
 			}
 			else if (!walker->u_next) { // if dnode is last node
 				walker->u_prev->u_next = NULL;
@@ -152,6 +190,7 @@ Event* delete_user(int uid, char* name) {
 		}
 		walker = walker->u_next;
 	}
+	u[uid].cnt--;
 
 	dnode->u_next = dnode->u_prev = NULL;
 	return dnode;
@@ -160,23 +199,88 @@ Event* delete_user(int uid, char* name) {
 /* USER FUNCTIONS END*/
 
 void init() {
-	sid = 0;
-	for (int i = 0; i < MAXH; i++) {
-		if (i < MAXN) {
-			u[i].cnt = 0;
-			u[i].events = NULL;
+	sid = tarray_cnt = 0;
+	for (register int g = 0; g < MAXG; g++) {
+		for (register int i = 0; i < MAXH; i++) {
+			if (i < MAXN) {
+				u[i].cnt = 0;
+				u[i].events = NULL;
+			}
+			htable[g][i] = NULL;
 		}
-		htable[i]->h_next = htable[i]->h_prev = htable[i]->u_next = htable[i]->h_prev = NULL;
 	}
 	return;
 }
 
 int changeEvent(int uid, char ename[], char cname[]) {
-	return 0;
+	int ccnt = 0;
+	bool isMaster = false;
+	register Event* walker = u[uid].events;
+	while (walker) {
+		if (!_strcmp(ename, walker->name)) {
+			break;
+		}
+		walker = walker->u_next;
+	}
+	if (!walker) return 0;
+
+	ccnt++;
+	walker = delete_hash(uid, walker->gid, ename);
+	_strcpy(walker->name, cname);
+	isMaster = walker->master;
+	insert_hash(walker->gid, walker);
+	if (!isMaster) return 1;
+
+	tarray_cnt = tarray[0] = tarray[1] = tarray[2] = tarray[3] = tarray[4] = 0;
+	register Event* walker_2 = walker;
+	while (walker_2) {
+		walker_2 = delete_hash2(walker_2->gid, ename);
+		if (walker_2) {
+			ccnt++;
+			tarray[tarray_cnt++] = walker_2->sid;
+		}
+	}
+
+	for (register int i = 0; i < tarray_cnt; i++) {
+		_strcpy(node[tarray[i]].name, cname);
+		if (i + 1 < tarray_cnt) {
+			node[tarray[i]].h_next = &node[tarray[i + 1]];
+			node[tarray[i + 1]].h_prev = &node[tarray[i]];
+		}
+	}
+	if (tarray_cnt > 0) {
+		walker->h_next = &node[tarray[0]];
+		node[tarray[0]].h_prev = walker;
+	}
+
+	return ccnt;
 }
 
 int deleteEvent(int uid, char ename[]) {
-	return 0;
+	int dcnt = 0;
+	Event* walker = u[uid].events;
+	while (walker) {
+		if (!_strcmp(ename, walker->name)) {
+			break;
+		}
+		walker = walker->u_next;
+	}
+	if (!walker) return 0;
+
+	dcnt++;
+	walker = delete_hash(uid, walker->gid, ename);
+	delete_user(uid, ename);
+	if (!walker->master) return 1;
+
+	while (walker) {
+		walker = delete_hash2(walker->gid, ename);
+		if (walker) {
+			delete_user(walker->uid, ename);
+			dcnt++;
+		}
+	}
+
+	return dcnt;
 }
 
 void addEvent(int uid, char ename[], int groupid) {
@@ -185,130 +289,13 @@ void addEvent(int uid, char ename[], int groupid) {
 	newNode->gid = groupid;
 	newNode->uid = uid;
 	newNode->master = false;
-	newNode->h_next = newNode->h_prev = newNode->u_next = newNode->h_prev = NULL;
+	newNode->h_next = newNode->h_prev = newNode->u_next = newNode->u_prev = NULL;
 	_strcpy(newNode->name, ename);
-	insert_hash(newNode);
+	insert_hash(groupid, newNode);
 	insert_user(uid, newNode);
 	return;
 }
 
 int getCount(int uid) {
 	return u[uid].cnt;
-}
-
-typedef enum {
-	INIT,
-	ADD,
-	DELETE,
-	CHANGE,
-	GETCOUNT
-} COMMAND;
-
-extern void init();
-extern void addEvent(int uid, char ename[], int groupid);
-extern int deleteEvent(int uid, char ename[]);
-extern int changeEvent(int uid, char ename[], char cname[]);
-extern int getCount(int uid);
-
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-static int mSeed;
-static int pseudo_rand(void)
-{
-	mSeed = mSeed * 431345 + 2531999;
-	return mSeed & 0x7FFFFFFF;
-}
-
-static void make_string(char str[], int seed)
-{
-	mSeed = seed;
-	int length = 5 + pseudo_rand() % 10;
-	for (int i = 0; i < length; ++i) {
-		str[i] = 'a' + pseudo_rand() % 26;
-	}
-	str[length] = 0;
-}
-
-static int run()
-{
-	int answer = 100;
-
-	int cmd, p1, p2, p3, p4;
-	char str1[15], str2[15];
-
-	int N, ret, flag;
-	scanf("%d %d %d", &cmd, &N, &flag);
-
-	init();
-
-	for (int i = 1; i < N; ++i) {
-		scanf("%d", &cmd);
-		switch (cmd) {
-		case ADD:
-			if (flag == 1) {
-				scanf("%d %s %d", &p1, str1, &p3);
-			}
-			else {
-				scanf("%d %d %d", &p1, &p2, &p3);
-				make_string(str1, p2);
-			}
-			addEvent(p1, str1, p3);
-			break;
-
-		case DELETE:
-			if (flag == 1) {
-				scanf("%d %s %d", &p1, str1, &p3);
-			}
-			else {
-				scanf("%d %d %d", &p1, &p2, &p3);
-				make_string(str1, p2);
-			}
-			ret = deleteEvent(p1, str1);
-			if (ret != p3)
-				answer = 0;
-			break;
-
-		case CHANGE:
-			if (flag == 1) {
-				scanf("%d %s %s %d", &p1, str1, str2, &p4);
-			}
-			else {
-				scanf("%d %d %d %d", &p1, &p2, &p3, &p4);
-				make_string(str1, p2);
-				make_string(str2, p3);
-			}
-			ret = changeEvent(p1, str1, str2);
-			if (ret != p4)
-				answer = 0;
-			break;
-
-		case GETCOUNT:
-			scanf("%d %d", &p1, &p2);
-			ret = getCount(p1);
-			if (ret != p2)
-				answer = 0;
-			break;
-
-		default:
-			break;
-		}
-	}
-
-	return answer;
-}
-
-int main()
-{
-	setbuf(stdout, NULL);
-	//freopen("sample_input.txt", "r", stdin);
-
-	int T;
-	scanf("%d", &T);
-
-	for (int tc = 1; tc <= T; tc++) {
-		printf("#%d %d\n", tc, run());
-	}
-
-	return 0;
 }
