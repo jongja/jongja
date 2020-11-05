01fd97e32bb5726f7bf1129d78483db2243aabe2
jongja <whwhdgus94@naver.com>
2020-11-05 23:03:44 +0900

    Upload new App

---
+++ Study/swift/applications/TabbarEX/TabbarEX/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  TabbarEX
+//
+//  Created by 종자 on 2018. 7. 18..
+//  Copyright © 2018년 Jongja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var FristLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        FristLabel.text = "     2번 버튼이 눌렸습니다."
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Study/swift/applications/TabbarEX/TabbarEX/ThirdViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.swift
+//  TabbarEX
+//
+//  Created by 종자 on 2018. 7. 18..
+//  Copyright © 2018년 Jongja. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var FristLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        FristLabel.text = "     3번 버튼이 눌렸습니다."
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
