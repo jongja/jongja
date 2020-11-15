6520ba87f4eb38077acda70924892477e5110670
jongja <whwhdgus94@naver.com>
2020-11-15 22:17:31 +0900

    Upload new application

---
+++ Study/swift/applications/PracticeAPP/PracticeAPP/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  PracticeAPP
+//
+//  Created by 종자 on 2018. 8. 9..
+//  Copyright © 2018년 Jongja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var subway: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
