3fa13cbc246551940e43e0a540650ed7f6d67c86
jongja <whwhdgus94@naver.com>
2021-02-23 00:00:34 +0900

    Update solution

--- Algorithm/SWEA/SWEA_H1914.cpp
+++ Algorithm/SWEA/SWEA_H1914.cpp
@@ -1,5 +1,5 @@
-/* Not Completed..
- * 
+/* Complete!
+ *
  */
 
 #ifndef _CRT_SECURE_NO_WARNINGS 
@@ -10,206 +10,387 @@
 
 #define MAX_N    100
 #define MAX_TOOL 50
-#define MAX_HEAP 4800
-
-int N;
-
-struct Tool {
-	int step;
-	int tid;
-	int pt;
-	int end_time;
-	bool hasLot;
-}t[MAX_TOOL];
-
-struct Heap {
-	int h[MAX_HEAP];
-	int size;
-	bool init_mode;
-
-	void init() {
-		size = 0;
-		init_mode = false;
-	}
-	void push(int tid) {
-		if (size == MAX_HEAP)
-			return;
-		int cur = ++size;
-		if (init_mode) {
-			while (cur != 1 && t[h[cur >> 1]].pt > t[tid].pt) {
-				h[cur] = h[cur >> 1];
-				cur >>= 1;
-			}
-		}
-		else {
-			while (cur != 1 && t[h[cur >> 1]].end_time > t[tid].end_time) {
-				h[cur] = h[cur >> 1];
-				cur >>= 1;
-			}
-		}
-		h[cur] = tid;
-	}
-
-	void pop() {
-		if (size == 0)
-			return;
-		int e = h[size--];
-		int cur = 1, ch = 2;
-
-		while (ch <= size) {
-			if (init_mode) {
-				if (ch < size && t[h[ch]].pt > t[h[ch + 1]].pt) ch++;
-				if (t[h[ch]].pt >= t[e].pt) break;
-			}
-			else {
-				if (ch < size && t[h[ch]].end_time > t[h[ch + 1]].end_time) ch++;
-				if (t[h[ch]].end_time >= t[e].end_time) break;
-			}
-			h[cur] = h[ch];
-			cur = ch; ch <<= 1;
-		}
-		h[cur] = e;
-	}
-
-	void init_push(int tid) {
-		init_mode = true;
-		push(tid);
-		init_mode = false;
-	}
-
-	int top() {
-		return h[1];
-	}
-};
-
-struct Step {
-	Heap pq;
-	int lot;
-}s[MAX_N];
-
-int tid, curTime, resultLot;
-
-void sim_start(int future_time) {
-	int top_tid = 0;
-	for(int i = 0; i < N; i++) {
-		top_tid = s[i].pq.top();
-		while (t[top_tid].end_time > 0 && t[top_tid].end_time <= future_time) {
-			if (i == N - 1) { // if this Step is last
-				s[i].pq.pop();
-				resultLot++;
-			}
-			else {
-				s[i].pq.pop();
-				s[i + 1].lot++;
-			}
-
-			if (s[i].lot > 0) { // if current step has lot, add to tool.
-				s[i].lot--;
-				t[top_tid].end_time = future_time + t[top_tid].pt;
-				s[i].pq.push(top_tid);
-			}
-
-			top_tid = s[i].pq.top();
-		}
-	}
-}
-
-void init(int N) {
-	::N = N;
-	tid = curTime = resultLot = 0;
-	for (int i = 0; i < N; i++) {
-		s[i].lot = 0;
-		s[i].pq.init();
-	}
-}
-
-void setupTool(int T, int stepNo[5000], int procTime[5000]) {
-	for (int i = 0; i < T; i++) {
-		t[i].hasLot = false;
-		t[i].pt = procTime[i];
-		t[i].end_time = 0;
-		t[i].step = stepNo[i];
-		t[i].tid = i;
-		s[stepNo[i]].pq.init_push(i);
-	}
-}
-
-void addLot(int time, int number) {
-	s[0].lot += number;
-	sim_start(curTime + time);
-	
-}
-
-int simulate(int time, int wip[MAX_N]) {
-	sim_start(curTime + time);
-	return 0;
-}
-
-
 
 #define ENDCASE 0
 #define ADDLOT  1
 #define SIMUL   2
 
-static int run() {
-	int totalStep = 0, totalTool = 0, stepID[MAX_N * MAX_TOOL], procTime[MAX_N * MAX_TOOL];
-	int event, time, number;
-	int userOut, userWip[MAX_N], resultOut, resultWip[MAX_N];
-	int score = 100;
+#define MAX_T 5001
+#define NULL 0
 
-	for (int j = 0; j < MAX_N; j++) {
-		userWip[j] = resultWip[j] = 0;
-	}
+int semiconductors = 0;
+int _N;
+int step;
 
-	scanf("%d %d", &totalStep, &totalTool);
-	init(totalStep);
+struct Eventnode {
+    int tool;
+    int step;
+    int tools_pt;
+    int time;
+};
 
-	for (register int i = 0; i < totalTool; i++) {
-		scanf("%d %d", stepID + i, procTime + i);
-	}
-	setupTool(totalTool, stepID, procTime);
+struct Tnode {
+    bool hasLot;
+    int pt;
+    int step;
+    Tnode* next;
+    Tnode* prev;
+}tnode[MAX_T];
 
-	while (1) {
-		scanf("%d", &event);
-		if (event == ADDLOT) {
-			scanf("%d %d", &time, &number);
-			addLot(time, number);
-		}
+struct Nnode
+{
+    int lots;
+    int working_lots;
+    Tnode* t;
+}nnode[MAX_N];
 
-		else if (event == SIMUL) {
-			scanf("%d", &time);
-			for (int j = 0; j < totalStep; j++) {
-				userWip[j] = 0;
-				scanf("%d", &resultWip[j]);
-			}
-			scanf("%d", &resultOut);
+bool canIpush(register Eventnode cur, register Eventnode parent) {
+    if (cur.time < parent.time) {
+        return true;
+    }
+    else if (cur.time > parent.time) {
+        return false;
+    }
+    else {
+        if (cur.tool > parent.tool) {
+            return true;
+        }
+        else if (cur.tool < parent.tool) {
+            return false;
+        }
+        else {
+            if (cur.tools_pt < parent.tools_pt) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
 
-			userOut = simulate(time, userWip);
+bool canIpop(register Eventnode cur, register Eventnode child) {
+    if (cur.time < child.time) {
+        return true;
+    }
+    else if (cur.time > child.time) {
+        return false;
+    }
+    else {
+        if (cur.tool < child.tool) {
+            return false;
+        }
+        else if (cur.tool > child.tool) {
+            return true;
+        }
+        else {
+            if (cur.tools_pt < child.tools_pt) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
 
-			for (int j = 0; j < totalStep; j++) {
-				if (userWip[j] != resultWip[j]) {
-					score = 0;
-					break;
-				}
-			}
-			if (userOut != resultOut) {
-				score = 0;
-			}
-		}
-		else {
-			return score;
-		}
-	}
-	return score;
+bool canIright(register Eventnode cur, register Eventnode right) {
+    if (cur.time > right.time) {
+        return true;
+    }
+    else if (cur.time < right.time) {
+        return false;
+    }
+    else {
+        if (cur.tool > right.tool) {
+            return false;
+        }
+        else if (cur.tool < right.tool) {
+            return true;
+        }
+        else {
+            if (cur.tools_pt > right.tools_pt) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+struct Heap {
+    int size;
+    struct Eventnode data[MAX_T];
+    void init() {
+        size = 0;
+    }
+    void push(Eventnode val) {
+        if (size == MAX_T) {
+            return;
+        }
+        register int cur = ++size;
+        while (cur != 1 && canIpush(val, data[cur >> 1])) {
+            data[cur] = data[cur >> 1];
+            cur >>= 1;
+        }
+        data[cur] = val;
+    }
+
+    void pop() {
+        if (!size)
+            return;
+        register Eventnode e = data[size--];
+        register int cur = 1, ch = 2;
+        while (ch <= size) {
+            if (ch < size && canIright(data[ch], data[ch + 1])) ch++;
+            if (canIpop(e, data[ch])) {
+                break;
+            }
+            data[cur] = data[ch];
+            cur = ch; ch <<= 1;
+        }
+        data[cur] = e;
+    }
+
+    Eventnode top() {
+        return data[1];
+    }
+
+}heap;
+
+void insert_list(int to, Tnode* newNode) {
+    register Tnode* walker = nnode[to].t;
+    bool pushed = false;
+    if (!walker) {
+        nnode[to].t = newNode;
+        pushed = true;
+    }
+    else {
+        while (walker) {
+            if (newNode->pt < walker->pt) {
+                if (!walker->prev) {
+                    newNode->next = walker;
+                    walker->prev = newNode;
+                    nnode[to].t = newNode;
+                    pushed = true;
+                    break;
+                }
+                else {
+                    newNode->next = walker;
+                    walker->prev->next = newNode;
+                    newNode->prev = walker->prev;
+                    walker->prev = newNode;
+                    pushed = true;
+                    break;
+                }
+            }
+            walker = walker->next;
+        }
+    }
+    if (!pushed) {
+        walker = nnode[to].t;
+        while (walker->next) walker = walker->next;
+        walker->next = newNode;
+        newNode->prev = walker;
+    }
+    return;
+}
+
+void init(int N) {
+    _N = N;
+    semiconductors = 0;
+    heap.init();
+    for (int i = 0; i < N + 1; i++) {
+        nnode[i].lots = nnode[i].working_lots = 0;
+        nnode[i].t = NULL;
+    }
+}
+
+void setupTool(int T, int stepNo[5000], int procTime[5000]) {
+    for (int i = 0; i < T; i++) {
+        tnode[i].hasLot = false;
+        tnode[i].next = NULL;
+        tnode[i].prev = NULL;
+        tnode[i].pt = procTime[i];
+        tnode[i].step = i;
+        insert_list(stepNo[i], &tnode[i]);
+    }
+}
+
+void startEvent() {
+    Eventnode cur = heap.top();
+    heap.pop();
+    int cur_time = cur.time;
+    int cur_tool = cur.tool;
+    int cur_step = cur.step;
+    int cur_pt = cur.tools_pt;
+    tnode[cur.step].hasLot = false;
+
+    if (cur_tool < _N - 1) {
+        nnode[cur_tool].working_lots--;
+        nnode[cur_tool + 1].lots++;
+        Tnode* walker = nnode[cur_tool + 1].t;
+        while (walker) {
+            if (!walker->hasLot) {
+                walker->hasLot = true;
+
+                cur.time += walker->pt;
+                cur.tool = cur_tool + 1;
+                cur.tools_pt = walker->pt;
+                cur.step = walker->step;
+
+                nnode[cur_tool + 1].lots--;
+                nnode[cur_tool + 1].working_lots++;
+                heap.push(cur);
+                break;
+            }
+            walker = walker->next;
+        }
+        if (nnode[cur_tool].lots) {
+            nnode[cur_tool].lots--;
+            nnode[cur_tool].working_lots++;
+            tnode[cur_step].hasLot = true;
+            cur.time = cur_time + cur_pt;
+            cur.tool = cur_tool;
+            cur.tools_pt = cur_pt;
+            cur.step = cur_step;
+            heap.push(cur);
+        }
+    }
+    else {
+        semiconductors++;
+        nnode[cur_tool].working_lots--;
+        if (nnode[cur_tool].lots) {
+            nnode[cur_tool].lots--;
+            nnode[cur_tool].working_lots++;
+            tnode[cur_step].hasLot = true;
+            cur.time = cur_time + cur_pt;
+            cur.tool = cur_tool;
+            cur.tools_pt = cur_pt;
+            cur.step = cur_step;
+            heap.push(cur);
+        }
+    }
+}
+
+void addLot(int time, int number) {
+    if (heap.size == 0) {
+        nnode[0].lots += number;
+        register Tnode* walker = nnode[0].t;
+        while (walker) {
+            if (nnode[0].lots && !walker->hasLot) {
+                register Eventnode cur;
+                walker->hasLot = true;
+
+                cur.time = walker->pt;
+                cur.tool = 0;
+                cur.tools_pt = walker->pt;
+                cur.step = walker->step;
+
+                nnode[0].working_lots++;
+                nnode[0].lots--;
+                heap.push(cur);
+
+            }
+            walker = walker->next;
+        }
+    }
+    else {
+        while (heap.top().time <= time) {
+            startEvent();
+        }
+        nnode[0].lots += number;
+        register Tnode* walker = nnode[0].t;
+        while (walker) {
+            if (nnode[0].lots && !walker->hasLot) {
+                register Eventnode cur;
+                walker->hasLot = true;
+
+                cur.time = time + walker->pt;
+                cur.tool = 0;
+                cur.tools_pt = walker->pt;
+                cur.step = walker->step;
+
+                nnode[0].working_lots++;
+                nnode[0].lots--;
+                heap.push(cur);
+            }
+            walker = walker->next;
+        }
+    }
+    return;
+}
+
+int simulate(int time, int wip[MAX_N]) {
+    while (heap.size && heap.top().time <= time) {
+        startEvent();
+    }
+    for (register int i = 0; i < _N; i++)
+        wip[i] = nnode[i].lots + nnode[i].working_lots;
+    return semiconductors;
+}
+
+int run() {
+    int totalStep = 0, totalTool = 0, stepID[MAX_N * MAX_TOOL], procTime[MAX_N * MAX_TOOL];
+    int event, time, number;
+    int userOut, userWip[MAX_N], resultOut, resultWip[MAX_N];
+    int score = 100;
+
+    for (int j = 0; j < MAX_N; j++) {
+        userWip[j] = resultWip[j] = 0;
+    }
+
+    scanf("%d %d", &totalStep, &totalTool);
+    init(totalStep);
+    step = 0;
+    for (register int i = 0; i < totalTool; i++) {
+        scanf("%d %d", stepID + i, procTime + i);
+    }
+    setupTool(totalTool, stepID, procTime);
+
+    while (1) {
+        step++;
+        scanf("%d", &event);
+        if (event == ADDLOT) {
+            scanf("%d %d", &time, &number);
+            addLot(time, number);
+        }
+
+        else if (event == SIMUL) {
+            scanf("%d", &time);
+            for (int j = 0; j < totalStep; j++) {
+                userWip[j] = 0;
+                scanf("%d", &resultWip[j]);
+            }
+            scanf("%d", &resultOut);
+
+            userOut = simulate(time, userWip);
+
+            for (int j = 0; j < totalStep; j++) {
+                if (userWip[j] != resultWip[j]) {
+                    score = 0;
+                    break;
+                }
+            }
+            if (userOut != resultOut) {
+                score = 0;
+            }
+        }
+        else {
+            return score;
+        }
+    }
+    return score;
 }
 
 int main() {
-	setbuf(stdout, NULL);
-	int TestCase;
+    setbuf(stdout, NULL);
+    int TestCase;
 
-	freopen("input.txt", "r", stdin); 
-	scanf("%d", &TestCase);
-	for (int t = 1; t <= TestCase; ++t)
-		printf("#%d %d\n", t, run());
-	return 0;
+    freopen("input.txt", "r", stdin);
+    scanf("%d", &TestCase);
+    for (int t = 1; t <= TestCase; ++t)
+        printf("#%d %d\n", t, run());
+    return 0;
 }
